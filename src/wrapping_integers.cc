3e01abc0d0df06848b96c35c87d647352ac16154
HyFren <hyfrencn@gmail.com>
2025-08-07 11:07:10 +0800

    complete part of check2: wrap and unwrap 完成check2的部分任务wrap和unwrap函数

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,18 +1,22 @@
 #include "wrapping_integers.hh"
 #include "debug.hh"
+#include <cstdint>
 
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32 { zero_point + n };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint64_t low_bits = static_cast<uint64_t>(raw_value_ - zero_point.raw_value_);
+  uint64_t high_bits_upper = ( checkpoint + ( 1UL << 31 ) ) & ( UINT64_MAX - UINT32_MAX);
+  uint64_t high_bits_lower = ( checkpoint - ( 1UL << 31 ) ) & ( UINT64_MAX - UINT32_MAX);
+  uint64_t upper = high_bits_upper | low_bits;
+  uint64_t lower = high_bits_lower | low_bits;
+  uint64_t distance_upper = max(checkpoint, upper) - min(checkpoint, upper);
+  uint64_t distance_lower = max(checkpoint, lower) - min(checkpoint, lower);
+  return distance_upper < distance_lower ? upper : lower;
 }
