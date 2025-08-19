75fc2bf095ad14e69a174ac61f63cc3234ed70fc
Zaden Yip <hyfrencn@gmail.com>
2025-08-20 01:15:36 +0800

    refactor: add finite state machien to manage TCPReceiver process

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -13,11 +13,37 @@ void TCPReceiver::receive( TCPSenderMessage message )
     reassembler_.reader().set_error();
     return;
   }
-  if ( message.SYN ) {
-    this->isn_ = message.seqno;
-    this->rcv_absolute_ack_seq_ = 0;
+  switch ( kReceiverState_ ) {
+    case ReceiverState::CLOSED:
+      closed_handler( message );
+      return;
+    case ReceiverState::ESTABLISHED:
+      established_handler( message );
+      break;
+    default:
+      break;
   }
-  uint64_t stream_seq = message.seqno.unwrap( isn_, rcv_absolute_ack_seq_ ) - ( message.SYN ? 0 : 1 );
+}
+
+void TCPReceiver::closed_handler( const TCPSenderMessage& msg )
+{
+  if ( msg.SYN ) {
+    this->isn_ = msg.seqno;
+    this->rcv_absolute_ack_seq_ = 0;
+    kReceiverState_ = ReceiverState::ESTABLISHED;
+    byte_push( msg );
+    rcv_absolute_ack_seq_ += ( msg.SYN + msg.FIN );
+  }
+}
+
+void TCPReceiver::established_handler( const TCPSenderMessage& msg )
+{
+  byte_push( msg );
+  if ( reassembler_.writer().is_closed() ) {
+    rcv_absolute_ack_seq_ += ( reassembler_.writer().is_closed() );
+    kReceiverState_ = ReceiverState::CLOSED;
+  }
+}
   uint64_t old_pushed_bytes = reassembler_.writer().bytes_pushed();
   reassembler_.insert( stream_seq, message.payload, message.FIN );
   uint64_t new_pushed_bytes = reassembler_.writer().bytes_pushed();
