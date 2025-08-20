63fa0512d6ab837ac8c07dd4ebada3bca8568a5f
HyFren <hyfrencn@gmail.com>
2025-08-20 15:58:35 +0800

    perf: Rework ByteStream buffer logic for better pop() performance 优化: 重构 ByteStream 缓冲区逻辑以获得更好的 pop() 性能

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,4 +1,5 @@
 #include "byte_stream.hh"
+#include <algorithm>
 #include <cstdint>
 #include <cstdio>
 #include <string>
@@ -6,20 +7,17 @@
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), stream_start_(0) {}
 
 void Writer::push( string data )
 {
-  string add_stream = data.substr( 0, available_capacity() );
-  stream_.append( add_stream );
-  write_byte_num_ += add_stream.size();
+  uint64_t add_num = min(available_capacity(), data.size());
+  buffer_.append(data, 0, add_num);
+  write_byte_num_ += add_num;
 }
 
 void Writer::close()
 {
-  if ( is_closed() ) {
-    return;
-  }
   closed_ = true;
 }
 
@@ -30,7 +28,7 @@ bool Writer::is_closed() const
 
 uint64_t Writer::available_capacity() const
 {
-  return capacity_ - stream_.size();
+  return capacity_ - ( buffer_.size() - stream_start_ );
 }
 
 uint64_t Writer::bytes_pushed() const
@@ -42,23 +40,27 @@ uint64_t Writer::bytes_pushed() const
 
 string_view Reader::peek() const
 {
-  return stream_; // Your code here.
+  return string_view(buffer_.data() + stream_start_, buffer_.size() - stream_start_);
 }
 
 void Reader::pop( uint64_t len )
 {
-  stream_ = stream_.substr( len );
+  stream_start_ += len;
+  if (stream_start_ >= buffer_.size() / 2) {
+    buffer_.erase(0, stream_start_);
+    stream_start_ = 0;
+  }
   read_byte_num_ += len;
 }
 
 bool Reader::is_finished() const
 {
-  return closed_ && stream_.size() == 0; // Your code here.
+  return closed_ && buffer_.size() - stream_start_ == 0 ; // Your code here.
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return stream_.size(); // Your code here.
+  return buffer_.size() - stream_start_; // Your code here.
 }
 
 uint64_t Reader::bytes_popped() const
