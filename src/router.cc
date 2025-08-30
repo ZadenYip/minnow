d6a3f44499a9aa9ed27d03a9215a6a2080d1903a
HyFren <hyfrencn@gmail.com>
2025-08-30 21:09:53 +0800

    feat(router): 实现 Trie 数据结构来管理路由和路由查询 / implement Trie data structure for route management and lookup

--- src/router.cc
+++ src/router.cc
@@ -1,7 +1,17 @@
 #include "router.hh"
+#include "address.hh"
 #include "debug.hh"
+#include "ipv4_datagram.hh"
+#include "network_interface.hh"
 
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
 #include <iostream>
+#include <memory>
+#include <optional>
+#include <queue>
+#include <utility>
 
 using namespace std;
 
@@ -26,5 +36,41 @@ void Router::add_route( const uint32_t route_prefix,
 // Go through all the interfaces, and route every incoming datagram to its proper outgoing interface.
 void Router::route()
 {
-  debug( "unimplemented route() called" );
+
+void Router::Trie::add_route_entry( const uint32_t& route_prefix,
+                            const uint8_t& prefix_length,
+                            const size_t& interface_num,
+                            const std::optional<Address>& next_hop )
+{
+  TrieNode* current = root.get();
+  for ( uint8_t i = 0; i < prefix_length; ++i ) {
+    bool bit = get_bit( route_prefix, i );
+    if ( current->children_[bit] == nullptr ) {
+      current->children_[bit] = std::make_unique<TrieNode>();
+    }
+    current = current->children_[bit].get();
+  }
+  current->interface_num_ = interface_num;
+  current->next_hop_ = next_hop;
+}
+
+std::pair<std::optional<size_t>, std::optional<Address>> Router::Trie::lookup( const uint32_t& target )
+{
+  TrieNode* current = root.get();
+  TrieNode* best_match_node = root.get();
+
+  for ( uint8_t i = 0; i < 32; ++i ) {
+    if ( current == nullptr ) {
+      break;
+    }
+
+    bool bit = get_bit( target, i );
+    current = current->children_[bit].get();
+
+    if ( current != nullptr && current->interface_num_.has_value() ) {
+      best_match_node = current;
+}
+  }
+
+  return { best_match_node->interface_num_, best_match_node->next_hop_ };
 }
